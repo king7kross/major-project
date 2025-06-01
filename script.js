84df61bbe847a9a762194d4afc94700f7ad4b5c5
king7kross <65103863+king7kross@users.noreply.github.com>
2025-06-01 22:42:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+let menu= document.querySelector('#menu-btn');
+let navbar= document.querySelector('.header .navbar');
+
+menu.onclick = () =>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+};
+
+window.onscroll = () =>{
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+};
+
+var swiper = new Swiper(".home-slider", {
+    loop:true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+});
+
+
+
+// Copy of original script.js content
+// Please replace this placeholder with the actual content of your script.js file
